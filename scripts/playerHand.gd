f17e9eb43939a6fe6b18d7a3a4eb36b8c1f854d4
LourencoFreire <lourencofreire3@gmail.com>
2025-12-27 12:31:52 +0000

    now card snap back to hand

--- scripts/playerHand.gd
+++ scripts/playerHand.gd
@@ -10,13 +10,17 @@ func _ready() -> void:
 	center_screen_x = get_viewport().size.x / 2
 
 func add_card_to_hand(card):
-	player_hand.insert(0, card)
-	update_hand_positions()
+	if card not in player_hand:
+		player_hand.insert(0, card)
+		update_hand_positions()
+	else:
+		animate_card_to_position(card, card.starting_position)
 
 func update_hand_positions():
 	for i in range(player_hand.size()):
 		var new_position = Vector2(calculate_card_position(i), HAND_Y_POSITION)
 		var card = player_hand[i]
+		card.starting_position = new_position
 		animate_card_to_position(card, new_position)
 
 func calculate_card_position(index):

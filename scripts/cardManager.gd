5c0939d15d1718bda977b3f62b29c6acb8b70884
LourencoFreire <lourencofreire3@gmail.com>
2026-01-11 21:37:44 +0000

    WE ARE SO BACK! silly healthbar added

--- scripts/cardManager.gd
+++ scripts/cardManager.gd
@@ -30,6 +30,8 @@ func finish_drag():
 		card_being_dragged.position = card_slot_found.position
 		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
 		card_slot_found.card_in_slot = true
+		card_slot_found.current_card = card_being_dragged
+		card_slot_found.take_damage()
 	else:
 		player_hand_reference.add_card_to_hand(card_being_dragged)
 	card_being_dragged = null
--- scripts/cardSlot.gd
+++ scripts/cardSlot.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
 var card_in_slot = false
+var current_card = null
+
+@onready var healthbar = $Healthbar
+@onready var card_manager = $"../cardManager"
+
+func _ready() -> void:
+	healthbar.init_health(75)
+
+func take_damage():
+	if card_in_slot:
+		healthbar.health -= 15
+		delete_card()
+
+func delete_card():
+	if current_card:
+		current_card.queue_free()
+		current_card = null
+	card_in_slot = false
